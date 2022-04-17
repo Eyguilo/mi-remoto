e8d5736e113f591befe5cad4f6f0f9cbd18f2769
Eyguilo <jaumeeltoro@gmail.com>
2022-04-17 20:25:53 +0200

    Commit de volumen_cubo.py desde la rama errores-de-valor.

--- volumen_cubo.py
+++ volumen_cubo.py
@@ -5,5 +5,8 @@ def volumen_cubo(n):
 
 valor_lado = 4
 
+if valor_lado < 0:
+    raise ValueError("No hagas eso, no tiene sentido introducir un lado negativo")
+
 print()
 print("El volumen de este cubo es: ", valor_lado*valor_lado*valor_lado, "m^3")

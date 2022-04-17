37bb3225fca4affb800ab52dd73a88112bcfb429
Eyguilo <jaumeeltoro@gmail.com>
2022-04-17 18:42:53 +0200

    Hemos agregado un Script que calcula, mediante una función, el volumen de un cubo.

---
+++ volumen_cubo.py
@@ -0,0 +1,10 @@
+
+def volumen_cubo(n):
+    volumen=n**3
+    return volumen
+
+valor_lado = 4
+
+print()
+print("El volumen de un cubo de lado", valor_lado, "es de",
+      volumen_cubo(valor_lado), ".")
